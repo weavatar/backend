6ebf061f77c62f2895d7102728273410bd1ca0d6
耗子 <i@haozi.net>
2023-05-05 23:11:11 +0800

    feat: 移除没用的验证码

--- app/http/requests/avatar/update_avatar_request.go
+++ app/http/requests/avatar/update_avatar_request.go
@@ -7,9 +7,6 @@ import (
 
 type UpdateAvatarRequest struct {
 	Avatar string `form:"avatar" json:"avatar"`
-
-	CaptchaID string `form:"captcha_id" json:"captcha_id"`
-	Captcha   string `form:"captcha" json:"captcha"`
 }
 
 func (r *UpdateAvatarRequest) Authorize(ctx http.Context) error {
@@ -18,22 +15,14 @@ func (r *UpdateAvatarRequest) Authorize(ctx http.Context) error {
 
 func (r *UpdateAvatarRequest) Rules(ctx http.Context) map[string]string {
 	return map[string]string{
-		"avatar":     "required|image",
-		"captcha_id": "required|string",
-		"captcha":    "required|len:6|number|captcha",
+		"avatar": "required|image",
 	}
 }
 
 func (r *UpdateAvatarRequest) Messages(ctx http.Context) map[string]string {
 	return map[string]string{
-		"avatar.required":     "头像不能为空",
-		"avatar.image":        "头像必须为图片",
-		"captcha_id.required": "图形验证码 ID 不能为空",
-		"captcha_id.string":   "图形验证码 ID 必须为字符串",
-		"captcha.required":    "图形验证码不能为空",
-		"captcha.len":         "图形验证码长度必须为 6 位",
-		"captcha.number":      "图形验证码必须为数字",
-		"captcha.captcha":     "图形验证码错误",
+		"avatar.required": "头像不能为空",
+		"avatar.image":    "头像必须为图片",
 	}
 }
 
