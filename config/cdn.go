0939fa7523cd9fa12fc8eb6c82531cb43c8058ca
耗子 <i@haozi.net>
2023-05-09 16:45:56 +0800

    feat: 配置顺序调整

--- config/cdn.go
+++ config/cdn.go
@@ -8,7 +8,12 @@ func init() {
 	config := facades.Config
 	config.Add("cdn", map[string]any{
 		"driver": "starshield",
-
+		// 星盾
+		"starshield": map[string]interface{}{
+			"access_key": config.Env("JDCLOUD_ACCESS_KEY", ""),
+			"secret_key": config.Env("JDCLOUD_SECRET_KEY", ""),
+			"zone_id":    config.Env("JDCLOUD_STARSHIELD_ZONE_ID", ""),
+		},
 		// 盾云CDN
 		"ddun": map[string]interface{}{
 			"api_key":    config.Env("CDN_DDUN_API_KEY", ""),
@@ -18,12 +23,6 @@ func init() {
 		"yundun": map[string]interface{}{
 			"username": config.Env("CDN_YUNDUN_USERNAME", ""),
 			"password": config.Env("CDN_YUNDUN_PASSWORD", ""),
-		},
-		// 星盾
-		"starshield": map[string]interface{}{
-			"access_key": config.Env("JDCLOUD_ACCESS_KEY", ""),
-			"secret_key": config.Env("JDCLOUD_SECRET_KEY", ""),
-			"zone_id":    config.Env("JDCLOUD_STARSHIELD_ZONE_ID", ""),
 		},
 	})
 }
