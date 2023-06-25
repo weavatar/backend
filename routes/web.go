c8280732b4ae780fee0c9b4ee807e2897ab2467e
耗子 <i@haozi.net>
2023-06-25 17:15:01 +0800

    fix: 暂时移除限流

--- routes/web.go
+++ routes/web.go
@@ -20,13 +20,13 @@ func Web() {
 	facades.Route().Get("avatar", avatarController.Avatar)        // 用于获取头像
 	facades.Route().Get("avatar/", avatarController.Avatar)       // 用于获取头像
 
-	facades.Route().Prefix("captcha").Middleware(frameworkmiddleware.Throttle("global")).Group(func(route route.Route) {
+	facades.Route().Prefix("captcha").Group(func(route route.Route) {
 		captchaController := controllers.NewCaptchaController()
 		route.Middleware(frameworkmiddleware.Throttle("captcha")).Get("image", captchaController.Image)
 		route.Middleware(frameworkmiddleware.Throttle("verify_code")).Post("sms", captchaController.Sms)
 		route.Middleware(frameworkmiddleware.Throttle("verify_code")).Post("email", captchaController.Email)
 	})
-	facades.Route().Prefix("user").Middleware(frameworkmiddleware.Throttle("global")).Group(func(route route.Route) {
+	facades.Route().Prefix("user").Group(func(route route.Route) {
 		userController := controllers.NewUserController()
 		route.Post("oauthLogin", userController.OauthLogin)
 		route.Post("oauthCallback", userController.OauthCallback)
@@ -35,7 +35,7 @@ func Web() {
 		route.Middleware(middleware.Jwt()).Post("logout", userController.Logout)
 		route.Middleware(middleware.Jwt()).Post("refresh", userController.Refresh)
 	})
-	facades.Route().Middleware(frameworkmiddleware.Throttle("global")).Group(func(route route.Route) {
+	facades.Route().Group(func(route route.Route) {
 		//route.Middleware(middleware.Jwt()).Resource("avatars", avatarController)
 		route.Middleware(middleware.Jwt()).Get("avatars", avatarController.Index)
 		route.Middleware(middleware.Jwt()).Post("avatars", avatarController.Store)
@@ -44,7 +44,7 @@ func Web() {
 		route.Middleware(middleware.Jwt()).Patch("avatars/{id}", avatarController.Update)
 		route.Middleware(middleware.Jwt()).Delete("avatars/{id}", avatarController.Destroy)
 	})
-	facades.Route().Middleware(frameworkmiddleware.Throttle("global")).Group(func(route route.Route) {
+	facades.Route().Group(func(route route.Route) {
 		appController := controllers.NewAppController()
 		//route.Middleware(middleware.Jwt()).Resource("apps", appController)
 		route.Middleware(middleware.Jwt()).Get("apps", appController.Index)
@@ -55,7 +55,7 @@ func Web() {
 		route.Middleware(middleware.Jwt()).Delete("apps/{id}", appController.Destroy)
 	})
 
-	facades.Route().Prefix("system").Middleware(frameworkmiddleware.Throttle("global")).Group(func(route route.Route) {
+	facades.Route().Prefix("system").Group(func(route route.Route) {
 		systemController := controllers.NewSystemController()
 		route.Middleware(middleware.Jwt()).Get("checkBind", systemController.CheckBind)
 		route.Get("cdnUsage", systemController.CdnUsage)
