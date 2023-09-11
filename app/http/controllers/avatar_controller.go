9251cabdea3cddf46a3b936e02287729cbc7f1e0
耗子 <i@haozi.net>
2023-09-11 20:09:48 +0800

    fix: 又大意了

--- app/http/controllers/avatar_controller.go
+++ app/http/controllers/avatar_controller.go
@@ -241,7 +241,7 @@ func (r *AvatarController) Update(ctx http.Context) http.Response {
 		return sanitize
 	}
 
-	hash := updateAvatarRequest.ID
+	hash := ctx.Request().Input("id")
 	if len(hash) != 32 {
 		return Error(ctx, http.StatusUnprocessableEntity, "头像哈希格式错误")
 	}
