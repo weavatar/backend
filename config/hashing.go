51a54cd47a11f2266858d896071332c7625e5afd
耗子 <i@haozi.net>
2023-11-28 15:09:18 +0000

    feat: CDN调整

--- config/hashing.go
+++ config/hashing.go
@@ -13,19 +13,8 @@ func init() {
 		// by the framework hash facade.
 		// Default driver is "bcrypt".
 		//
-		// Supported Drivers: "argon2id", "bcrypt"
-		"driver": "argon2id",
-
-		// Argon2id Hashing Options
-		// memory: A memory cost, which defines the memory usage, given in kibibytes.
-		// time: A time cost, which defines the amount of computation
-		// realized and therefore the execution time, given in number of iterations.
-		// threads: A parallelism degree, which defines the number of parallel threads.
-		"argon2id": map[string]any{
-			"memory":  131072,
-			"time":    4,
-			"threads": 8,
-		},
+		// Supported Drivers: "bcrypt", "argon2id"
+		"driver": "bcrypt",
 
 		// Bcrypt Hashing Options
 		// rounds: The cost factor that should be used to compute the bcrypt hash.
@@ -34,7 +23,18 @@ func init() {
 		// factor by 1 doubles the necessary time. After a certain point, the returns on
 		// hashing time versus attacker time are diminishing, so choose your cost factor wisely.
 		"bcrypt": map[string]any{
-			"rounds": 10,
+			"rounds": 12,
+		},
+
+		// Argon2id Hashing Options
+		// memory: A memory cost, which defines the memory usage, given in kibibytes.
+		// time: A time cost, which defines the amount of computation
+		// realized and therefore the execution time, given in number of iterations.
+		// threads: A parallelism degree, which defines the number of parallel threads.
+		"argon2id": map[string]any{
+			"memory":  65536,
+			"time":    4,
+			"threads": 1,
 		},
 	})
 }
