c34229aa2de51cca5403d50aa0335adc346433d8
耗子 <i@haozi.net>
2024-07-10 01:30:15 +0800

    fix: captcha

--- app/rules/geetest.go
+++ app/rules/geetest.go
@@ -4,6 +4,8 @@ import (
 	"github.com/goravel-kit/geetest"
 	geetestfacades "github.com/goravel-kit/geetest/facades"
 	"github.com/goravel/framework/contracts/validation"
+	"github.com/goravel/framework/support/debug"
+	"github.com/goravel/framework/support/json"
 	"github.com/spf13/cast"
 )
 
@@ -17,27 +19,30 @@ func (receiver *Geetest) Signature() string {
 
 // Passes Determine if the validation rule passes.
 func (receiver *Geetest) Passes(data validation.Data, val any, options ...any) bool {
-	captcha, err := cast.ToStringMapStringE(val)
-	if err != nil {
+	debug.Dump(val)
+	if t, ok := val.(geetest.Ticket); ok {
+		verify, _ := geetestfacades.Geetest().Verify(t)
+		return verify
+	}
+
+	ticket := make(map[string]string)
+	if err := json.UnmarshalString(cast.ToString(val), &ticket); err != nil {
 		return false
 	}
 
 	keys := []string{"lot_number", "captcha_output", "pass_token", "gen_time"}
 	for _, key := range keys {
-		if _, ok := captcha[key]; !ok {
+		if _, ok := ticket[key]; !ok {
 			return false
 		}
 	}
 
-	verify, err := geetestfacades.Geetest().Verify(geetest.Ticket{
-		LotNumber:     captcha["lot_number"],
-		CaptchaOutput: captcha["captcha_output"],
-		PassToken:     captcha["pass_token"],
-		GenTime:       captcha["gen_time"],
+	verify, _ := geetestfacades.Geetest().Verify(geetest.Ticket{
+		LotNumber:     ticket["lot_number"],
+		CaptchaOutput: ticket["captcha_output"],
+		PassToken:     ticket["pass_token"],
+		GenTime:       ticket["gen_time"],
 	})
-	if err != nil {
-		return false
-	}
 
 	return verify
 }
